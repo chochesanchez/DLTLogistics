efa790a913f1a4b6bd0000e0f6229908a87d154a
Choche Sanchez <chochesanchez2003@gmail.com>
2025-11-18 15:27:12 -0600

    chore: complete cleanup - migrate all logos to public, remove unused dirs/files/deps

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,35 +3,7 @@
 import Reveal from '@/components/ui/Reveal'
 import PageHeader from '@/components/ui/PageHeader'
 import Image from 'next/image'
-import logoSrc from '@/../IMAGES/DLT Logistics LOGO.png'
 import { UserGroupIcon } from '@heroicons/react/24/outline'
-// partner logos (static imports)
-import whirlpoolLogo from '@/../IMAGES/Whirlpool Logo.png'
-import carrierLogo from '@/../IMAGES/Carrier Corporation Logo copy.png'
-import magnaLogo from '@/../IMAGES/Magna Logo copy.png'
-import usMotorsLogo from '@/../IMAGES/US Motors Logo copy.webp'
-import rxnLogo from '@/../IMAGES/RXN logo copy.png'
-import regalBeloitLogo from '@/../IMAGES/Regal Beloit Logo copy.png'
-import fluidmasterLogo from '@/../IMAGES/fluidmaster logo copy.png'
-import tuopuLogo from '@/../IMAGES/Tuopu Group Logo copy.jpeg'
-import superiorIndustriesLogo from '@/../IMAGES/superior industries logo copy.png'
-import dubaiLogo from '@/../IMAGES/Dubai Logo Dark copy.png'
-import dometicLogo from '@/../IMAGES/Dometic Logo copy.webp'
-import erdMetalCanadaLogo from '@/../IMAGES/erd-metal-canada logo copy.png'
-import nskJapanLogo from '@/../IMAGES/NSK Japan Logo copy.webp'
-import autozoneLogo from '@/../IMAGES/Autozone Logo copy.png'
-import motoradLogo from '@/../IMAGES/MotoRad Logo copy.webp'
-import metalsaLogo from '@/../IMAGES/Metalsa Logo copy.avif'
-import pilgrimsLogo from '@/../IMAGES/Pilgrims Logo copy.png'
-import muntersLogo from '@/../IMAGES/Munters Logo copy.webp'
-import nordicLogo from '@/../IMAGES/nordic logo copy.jpg'
-import terniumLogo from '@/../IMAGES/Ternium Logo copy.png'
-import magnesitasLogo from '@/../IMAGES/Magnesitas Logo copy.png'
-import mitsubaLogo from '@/../IMAGES/Mitsuba Corporation Logo copy.png'
-import elematecLogo from '@/../IMAGES/elematec logo copy.gif'
-import daimlerTrucksLogo from '@/../IMAGES/Daimler Trucks Logo copy.png'
-import conmetLogo from '@/../IMAGES/Conmet Logo copy.png'
-import toyotaLogo from '@/../IMAGES/Toyota Logo copy.png'
 
 export default function AboutPage() {
   return (
@@ -41,7 +13,7 @@ export default function AboutPage() {
       <div className="max-w-4xl mx-auto px-6 py-16 space-y-8">
         <Reveal as="h2" className="text-2xl md:text-3xl font-bold text-gray-900">We are</Reveal>
         <Reveal as="div" className="flex items-center gap-2">
-          <Image src={logoSrc} alt="DLT Logistics Logo" width={40} height={40} className="h-10 w-10 object-contain" priority />
+          <Image src="/images/DLT Logistics LOGO.png" alt="DLT Logistics Logo" width={40} height={40} className="h-10 w-10 object-contain" priority />
           <span className="text-3xl md:text-4xl font-extrabold text-[#FFCC00]">DLT</span>
           <span className="text-3xl md:text-4xl font-extrabold text-gray-800">Logistics</span>
         </Reveal>
@@ -87,32 +59,32 @@ export default function AboutPage() {
             '/images/partners/Embraco Logo.png',
             '/images/partners/sanminalogo.png',
             '/images/partners/Celestica Logo.png',
-            (whirlpoolLogo as unknown as string),
-            (carrierLogo as unknown as string),
-            (magnaLogo as unknown as string),
-            (usMotorsLogo as unknown as string),
-            (rxnLogo as unknown as string),
-            (regalBeloitLogo as unknown as string),
-            (fluidmasterLogo as unknown as string),
-            (tuopuLogo as unknown as string),
-            (superiorIndustriesLogo as unknown as string),
-            (dubaiLogo as unknown as string),
-            (dometicLogo as unknown as string),
-            (erdMetalCanadaLogo as unknown as string),
-            (nskJapanLogo as unknown as string),
-            (autozoneLogo as unknown as string),
-            (motoradLogo as unknown as string),
-            (metalsaLogo as unknown as string),
-            (pilgrimsLogo as unknown as string),
-            (muntersLogo as unknown as string),
-            (nordicLogo as unknown as string),
-            (terniumLogo as unknown as string),
-            (magnesitasLogo as unknown as string),
-            (mitsubaLogo as unknown as string),
-            (elematecLogo as unknown as string),
-            (daimlerTrucksLogo as unknown as string),
-            (conmetLogo as unknown as string),
-            (toyotaLogo as unknown as string),
+            '/images/partners/Whirlpool Logo.png',
+            '/images/partners/Carrier Corporation Logo.png',
+            '/images/partners/Magna Logo.png',
+            '/images/partners/US Motors Logo.webp',
+            '/images/partners/RXN logo.png',
+            '/images/partners/Regal Beloit Logo.png',
+            '/images/partners/fluidmaster logo.png',
+            '/images/partners/Tuopu Group Logo.jpeg',
+            '/images/partners/superior industries logo.png',
+            '/images/partners/Dubai Logo Dark.png',
+            '/images/partners/Dometic Logo.webp',
+            '/images/partners/erd-metal-canada logo.png',
+            '/images/partners/NSK Japan Logo.webp',
+            '/images/partners/Autozone Logo.png',
+            '/images/partners/MotoRad Logo.webp',
+            '/images/partners/Metalsa Logo.avif',
+            '/images/partners/Pilgrims Logo.png',
+            '/images/partners/Munters Logo.webp',
+            '/images/partners/nordic logo.jpg',
+            '/images/partners/Ternium Logo.png',
+            '/images/partners/Magnesitas Logo.png',
+            '/images/partners/Mitsuba Corporation Logo.png',
+            '/images/partners/elematec logo.gif',
+            '/images/partners/Daimler Trucks Logo.png',
+            '/images/partners/Conmet Logo.png',
+            '/images/partners/Toyota Logo.png',
           ].map((src, i) => (
             <div key={i} className="partner-card">
               <div className="relative w-40 h-20">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,37 +3,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Reveal from '@/components/ui/Reveal'
-import ContactForm from '@/components/ui/ContactForm'
 import BusinessContactForm from '@/components/ui/BusinessContactForm'
-import logoSrc from '@/../IMAGES/DLT Logistics LOGO.png'
-import whirlpoolLogo from '@/../IMAGES/Whirlpool Logo.png'
-import carrierLogo from '@/../IMAGES/Carrier Corporation Logo copy.png'
-import magnaLogo from '@/../IMAGES/Magna Logo copy.png'
-import usMotorsLogo from '@/../IMAGES/US Motors Logo copy.webp'
-import rxnLogo from '@/../IMAGES/RXN logo copy.png'
-import regalBeloitLogo from '@/../IMAGES/Regal Beloit Logo copy.png'
-import fluidmasterLogo from '@/../IMAGES/fluidmaster logo copy.png'
-import tuopuLogo from '@/../IMAGES/Tuopu Group Logo copy.jpeg'
-import superiorIndustriesLogo from '@/../IMAGES/superior industries logo copy.png'
-import dubaiLogo from '@/../IMAGES/Dubai Logo Dark copy.png'
-import dometicLogo from '@/../IMAGES/Dometic Logo copy.webp'
-import erdMetalCanadaLogo from '@/../IMAGES/erd-metal-canada logo copy.png'
-import nskJapanLogo from '@/../IMAGES/NSK Japan Logo copy.webp'
-import autozoneLogo from '@/../IMAGES/Autozone Logo copy.png'
-import motoradLogo from '@/../IMAGES/MotoRad Logo copy.webp'
-import metalsaLogo from '@/../IMAGES/Metalsa Logo copy.avif'
-import pilgrimsLogo from '@/../IMAGES/Pilgrims Logo copy.png'
-import muntersLogo from '@/../IMAGES/Munters Logo copy.webp'
-import nordicLogo from '@/../IMAGES/nordic logo copy.jpg'
-import terniumLogo from '@/../IMAGES/Ternium Logo copy.png'
-import magnesitasLogo from '@/../IMAGES/Magnesitas Logo copy.png'
-import mitsubaLogo from '@/../IMAGES/Mitsuba Corporation Logo copy.png'
-import elematecLogo from '@/../IMAGES/elematec logo copy.gif'
-import daimlerTrucksLogo from '@/../IMAGES/Daimler Trucks Logo copy.png'
-import conmetLogo from '@/../IMAGES/Conmet Logo copy.png'
-import toyotaLogo from '@/../IMAGES/Toyota Logo copy.png'
 // Using pure CSS marquee for smoothness; user pan was causing micro-jitters
-import heroTruck from '@/../IMAGES/hero-truck.png'
 import React from 'react'
 import { ShieldCheckIcon, ChartBarIcon, TruckIcon } from '@heroicons/react/24/outline'
  
@@ -71,7 +42,7 @@ export default function Home() {
       {/* Hero Section */}
       <div className='relative h-[620px] overflow-hidden'>
         <Image
-          src={heroTruck as unknown as string}
+          src="/images/hero-truck.png"
           alt='DLT Logistics Hero'
           fill
           style={{ objectFit: 'cover', objectPosition: 'center bottom' }}
@@ -130,7 +101,7 @@ export default function Home() {
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-8 pb-10'>
         <div className='text-gray-700'>
           <div className='mt-2 flex items-center gap-2 justify-center'>
-            <Image src={logoSrc} alt='DLT Logistics Logo' width={40} height={40} className='h-10 w-10 object-contain' />
+            <Image src='/images/DLT Logistics LOGO.png' alt='DLT Logistics Logo' width={40} height={40} className='h-10 w-10 object-contain' />
             <span className='text-3xl md:text-4xl font-extrabold text-[#FFCC00]'>DLT</span>
             <span className='text-3xl md:text-4xl font-extrabold text-gray-800'>Logistics</span>
           </div>
@@ -186,32 +157,32 @@ export default function Home() {
                 { name: 'Embraco', image: '/images/partners/Embraco Logo.png' },
                 { name: 'Sanmina', image: '/images/partners/sanminalogo.png' },
                 { name: 'Celestica', image: '/images/partners/Celestica Logo.png' },
-                { name: 'Whirlpool', image: (whirlpoolLogo as unknown as string) },
-                { name: 'Carrier', image: (carrierLogo as unknown as string) },
-                { name: 'Magna', image: (magnaLogo as unknown as string) },
-                { name: 'US Motors', image: (usMotorsLogo as unknown as string) },
-                { name: 'RXN', image: (rxnLogo as unknown as string) },
-                { name: 'Regal Beloit', image: (regalBeloitLogo as unknown as string) },
-                { name: 'Fluidmaster', image: (fluidmasterLogo as unknown as string) },
-                { name: 'Tuopu Group', image: (tuopuLogo as unknown as string) },
-                { name: 'Superior Industries', image: (superiorIndustriesLogo as unknown as string) },
-                { name: 'Dubai', image: (dubaiLogo as unknown as string) },
-                { name: 'Dometic', image: (dometicLogo as unknown as string) },
-                { name: 'ERD Metal Canada', image: (erdMetalCanadaLogo as unknown as string) },
-                { name: 'NSK Japan', image: (nskJapanLogo as unknown as string) },
-                { name: 'AutoZone', image: (autozoneLogo as unknown as string) },
-                { name: 'MotoRad', image: (motoradLogo as unknown as string) },
-                { name: 'Metalsa', image: (metalsaLogo as unknown as string) },
-                { name: 'Pilgrim’s', image: (pilgrimsLogo as unknown as string) },
-                { name: 'Munters', image: (muntersLogo as unknown as string) },
-                { name: 'Nordic', image: (nordicLogo as unknown as string) },
-                { name: 'Ternium', image: (terniumLogo as unknown as string) },
-                { name: 'Magnesitas', image: (magnesitasLogo as unknown as string) },
-                { name: 'Mitsuba', image: (mitsubaLogo as unknown as string) },
-                { name: 'Elematec', image: (elematecLogo as unknown as string) },
-                { name: 'Daimler Trucks', image: (daimlerTrucksLogo as unknown as string) },
-                { name: 'ConMet', image: (conmetLogo as unknown as string) },
-                { name: 'Toyota', image: (toyotaLogo as unknown as string) },
+                { name: 'Whirlpool', image: '/images/partners/Whirlpool Logo.png' },
+                { name: 'Carrier', image: '/images/partners/Carrier Corporation Logo.png' },
+                { name: 'Magna', image: '/images/partners/Magna Logo.png' },
+                { name: 'US Motors', image: '/images/partners/US Motors Logo.webp' },
+                { name: 'RXN', image: '/images/partners/RXN logo.png' },
+                { name: 'Regal Beloit', image: '/images/partners/Regal Beloit Logo.png' },
+                { name: 'Fluidmaster', image: '/images/partners/fluidmaster logo.png' },
+                { name: 'Tuopu Group', image: '/images/partners/Tuopu Group Logo.jpeg' },
+                { name: 'Superior Industries', image: '/images/partners/superior industries logo.png' },
+                { name: 'Dubai', image: '/images/partners/Dubai Logo Dark.png' },
+                { name: 'Dometic', image: '/images/partners/Dometic Logo.webp' },
+                { name: 'ERD Metal Canada', image: '/images/partners/erd-metal-canada logo.png' },
+                { name: 'NSK Japan', image: '/images/partners/NSK Japan Logo.webp' },
+                { name: 'AutoZone', image: '/images/partners/Autozone Logo.png' },
+                { name: 'MotoRad', image: '/images/partners/MotoRad Logo.webp' },
+                { name: 'Metalsa', image: '/images/partners/Metalsa Logo.avif' },
+                { name: 'Pilgrim's', image: '/images/partners/Pilgrims Logo.png' },
+                { name: 'Munters', image: '/images/partners/Munters Logo.webp' },
+                { name: 'Nordic', image: '/images/partners/nordic logo.jpg' },
+                { name: 'Ternium', image: '/images/partners/Ternium Logo.png' },
+                { name: 'Magnesitas', image: '/images/partners/Magnesitas Logo.png' },
+                { name: 'Mitsuba', image: '/images/partners/Mitsuba Corporation Logo.png' },
+                { name: 'Elematec', image: '/images/partners/elematec logo.gif' },
+                { name: 'Daimler Trucks', image: '/images/partners/Daimler Trucks Logo.png' },
+                { name: 'ConMet', image: '/images/partners/Conmet Logo.png' },
+                { name: 'Toyota', image: '/images/partners/Toyota Logo.png' },
               // duplicate for seamless loop
                  { name: 'BBVA', image: '/images/partners/bbvalogo.png' },
                  { name: 'Office Depot', image: '/images/partners/Office Depot Logo.webp' },
@@ -221,32 +192,32 @@ export default function Home() {
                  { name: 'Embraco', image: '/images/partners/Embraco Logo.png' },
                  { name: 'Sanmina', image: '/images/partners/sanminalogo.png' },
                  { name: 'Celestica', image: '/images/partners/Celestica Logo.png' },
-                 { name: 'Whirlpool', image: (whirlpoolLogo as unknown as string) },
-                 { name: 'Carrier', image: (carrierLogo as unknown as string) },
-                 { name: 'Magna', image: (magnaLogo as unknown as string) },
-                 { name: 'US Motors', image: (usMotorsLogo as unknown as string) },
-                 { name: 'RXN', image: (rxnLogo as unknown as string) },
-                 { name: 'Regal Beloit', image: (regalBeloitLogo as unknown as string) },
-                 { name: 'Fluidmaster', image: (fluidmasterLogo as unknown as string) },
-                 { name: 'Tuopu Group', image: (tuopuLogo as unknown as string) },
-                 { name: 'Superior Industries', image: (superiorIndustriesLogo as unknown as string) },
-                 { name: 'Dubai', image: (dubaiLogo as unknown as string) },
-                 { name: 'Dometic', image: (dometicLogo as unknown as string) },
-                 { name: 'ERD Metal Canada', image: (erdMetalCanadaLogo as unknown as string) },
-                 { name: 'NSK Japan', image: (nskJapanLogo as unknown as string) },
-                 { name: 'AutoZone', image: (autozoneLogo as unknown as string) },
-                 { name: 'MotoRad', image: (motoradLogo as unknown as string) },
-                 { name: 'Metalsa', image: (metalsaLogo as unknown as string) },
-                 { name: 'Pilgrim’s', image: (pilgrimsLogo as unknown as string) },
-                 { name: 'Munters', image: (muntersLogo as unknown as string) },
-                 { name: 'Nordic', image: (nordicLogo as unknown as string) },
-                 { name: 'Ternium', image: (terniumLogo as unknown as string) },
-                 { name: 'Magnesitas', image: (magnesitasLogo as unknown as string) },
-                 { name: 'Mitsuba', image: (mitsubaLogo as unknown as string) },
-                 { name: 'Elematec', image: (elematecLogo as unknown as string) },
-                 { name: 'Daimler Trucks', image: (daimlerTrucksLogo as unknown as string) },
-                 { name: 'ConMet', image: (conmetLogo as unknown as string) },
-                 { name: 'Toyota', image: (toyotaLogo as unknown as string) },
+                 { name: 'Whirlpool', image: '/images/partners/Whirlpool Logo.png' },
+                 { name: 'Carrier', image: '/images/partners/Carrier Corporation Logo.png' },
+                 { name: 'Magna', image: '/images/partners/Magna Logo.png' },
+                 { name: 'US Motors', image: '/images/partners/US Motors Logo.webp' },
+                 { name: 'RXN', image: '/images/partners/RXN logo.png' },
+                 { name: 'Regal Beloit', image: '/images/partners/Regal Beloit Logo.png' },
+                 { name: 'Fluidmaster', image: '/images/partners/fluidmaster logo.png' },
+                 { name: 'Tuopu Group', image: '/images/partners/Tuopu Group Logo.jpeg' },
+                 { name: 'Superior Industries', image: '/images/partners/superior industries logo.png' },
+                 { name: 'Dubai', image: '/images/partners/Dubai Logo Dark.png' },
+                 { name: 'Dometic', image: '/images/partners/Dometic Logo.webp' },
+                 { name: 'ERD Metal Canada', image: '/images/partners/erd-metal-canada logo.png' },
+                 { name: 'NSK Japan', image: '/images/partners/NSK Japan Logo.webp' },
+                 { name: 'AutoZone', image: '/images/partners/Autozone Logo.png' },
+                 { name: 'MotoRad', image: '/images/partners/MotoRad Logo.webp' },
+                 { name: 'Metalsa', image: '/images/partners/Metalsa Logo.avif' },
+                 { name: 'Pilgrim's', image: '/images/partners/Pilgrims Logo.png' },
+                 { name: 'Munters', image: '/images/partners/Munters Logo.webp' },
+                 { name: 'Nordic', image: '/images/partners/nordic logo.jpg' },
+                 { name: 'Ternium', image: '/images/partners/Ternium Logo.png' },
+                 { name: 'Magnesitas', image: '/images/partners/Magnesitas Logo.png' },
+                 { name: 'Mitsuba', image: '/images/partners/Mitsuba Corporation Logo.png' },
+                 { name: 'Elematec', image: '/images/partners/elematec logo.gif' },
+                 { name: 'Daimler Trucks', image: '/images/partners/Daimler Trucks Logo.png' },
+                 { name: 'ConMet', image: '/images/partners/Conmet Logo.png' },
+                 { name: 'Toyota', image: '/images/partners/Toyota Logo.png' },
               ].map((partner, index) => (
                 <div key={index} className='partner-card'>
                   <div className='relative w-40 h-20'>
